28df9e7f61ebb79bee8d8af19cd71d1057f5992d
Arthur Breurkes <arthurbreurkes@gmail.com>
2020-05-13 17:41:16 +0200

    Restrict location concept.

--- src/components/pages/Tasks.vue
+++ src/components/pages/Tasks.vue
@@ -9,37 +9,113 @@
 
             <md-tab id="tab-map" class="tab-one" md-label="Map" :md-template-data="{icon: 'map'}"
                     to="/tasks/tabOne">
-                <TasksTabOne></TasksTabOne>
+                <GmapMap
+                        Map
+                        :center="location"
+                        :zoom="18"
+                        :options="{
+                    disableDefaultUI: true,
+                    draggable: false,
+                    clickableIcons: false,
+                    streetViewControl: true
+                }"
+                        map-type-id="terrain"
+                        class="map"
+                        ref="mapRef"
+                >
+                </GmapMap>
             </md-tab>
             <md-tab id="tab-two" md-label="Street View" :md-template-data="{icon: 'streetview'}"
                     to="/tasks/tabTwo">
-                <TasksTabTwo></TasksTabTwo>
+                <div ref="pano" class="map"></div>
             </md-tab>
         </md-tabs>
         <hr class="tab-divider"/>
     </div>
 </template>
 <script>
-    import TasksTabOne from "../tabs/TasksTabOne";
-    import TasksTabTwo from "../tabs/TasksTabTwo";
+    import {gmapApi} from "vue2-google-maps";
 
     export default {
         name: 'Tasks',
-        components: {
-            TasksTabOne,
-            TasksTabTwo
-        },
+        components: {},
         data: function () {
-            return {}
+            return {
+                location: {lat:52.0116596, lng:4.3591061},
+                previousPosition: this.location,
+                polygons: [],
+                outerCoords: [{lat: 90,lng:-90},{lat: 90,lng: 90},{lat: 90,lng:180},{lat: 90,lng:-90},
+                    {lat: -90, lng: -90}, {lat: -90, lng: 180}, {lat: -90, lng: 90}, {lat: -90, lng: -90}],
+                innerCoords: [{lng: 4.3583259998, lat: 52.0109693785},
+                    {lng: 4.3581046768, lat: 52.0113134101},
+                    {lng: 4.357844388, lat: 52.0112506756},
+                    {lng: 4.3576394193, lat: 52.0115475968},
+                    {lng: 4.3599021447, lat: 52.0124058491},
+                    {lng: 4.3603868922, lat: 52.011960599},
+                    {lng: 4.360777354, lat: 52.0119813154},
+                    {lng: 4.3607659477, lat: 52.0118230383},
+                    {lng: 4.3605600584, lat: 52.0117503003},
+                    {lng: 4.3603482861, lat: 52.0118836737},
+                    {lng: 4.3594911572, lat: 52.0115163537},
+                    {lng: 4.3596133042, lat: 52.0114008894},
+                    {lng: 4.3595244125, lat: 52.0113659809},
+                    {lng: 4.3593954908, lat: 52.0114923702},
+                    {lng: 4.3583433611, lat: 52.0110958044},
+                    {lng: 4.3583865843, lat: 52.0109914234},
+                    {lng: 4.3583259998, lat: 52.0109693785}]
+            }
         },
-        props: {},
-        methods: {},
+        computed: {
+            google: gmapApi
+        },
         mounted: function () {
             this.$nextTick(() => {
                 if (this.$route.path === "/tasks/") {
                     this.$router.push("/tasks/tabOne")
                 }
+                this.initMap();
             });
+        },
+        methods: {
+            initMap: function () {
+                this.$refs.mapRef.$mapPromise.then((map) => {
+                    new this.google.maps.Polygon({
+                        paths: this.innerCoords,
+                        strokeColor: '#0000FF',
+                        strokeOpacity: 0.3,
+                        strokeWeight: 1,
+                        map: map,
+                        fillOpacity: 0.0
+                    });
+                    new this.google.maps.Polygon({
+                        paths: [this.outerCoords, this.innerCoords.reverse()],
+                        strokeWeight: 0,
+                        map: map,
+                        fillColor: '#FF0000',
+                        fillOpacity: 0.35
+                    });
+                    var pano = new this.google.maps.StreetViewPanorama(this.$refs.pano, {
+                        position: this.location
+                    });
+                    map.setStreetView(pano);
+
+                    this.polygons[0] = new this.google.maps.Polygon({
+                        paths: this.innerCoords,
+                        fillColor: '#FF0000',
+                        fillOpacity: 0
+                    });
+
+                    pano.addListener('position_changed', () => {
+                        var pos = pano.getPosition();
+
+                        if (pos !== this.previousPosition && !this.google.maps.geometry.poly.containsLocation(pos, this.polygons[0])) {
+                            pano.setPosition(this.previousPosition);
+                        } else {
+                            this.previousPosition = pos;
+                        }
+                    });
+                });
+            }
         }
     };
 </script>
@@ -92,5 +168,11 @@
 
     .md-tabs .md-tabs-navigation .md-button .md-icon {
         color: var(--forest-green) !important;
+    }
+
+    .map {
+        margin: -16px 0 0 -16px;
+        height: calc(100% + 32px);
+        width: calc(100% + 32px);
     }
 </style>
